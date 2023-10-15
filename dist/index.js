cc94b88fe1af98a01938487af2843d5a4e286669
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-10-15 20:22:24 +0300

    ez

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (button && input) {
+    button.addEventListener('click', () => {
+        input.value = 'hello!';
+    });
+}
+//# sourceMappingURL=index.js.map
